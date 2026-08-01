6adc797ce730e3c2110da989f3636deae02fb9a0
theeightboys <theboysofficialone@gmail.com>
2026-08-02 03:56:12 +0530

    Untrack .next, add .easignore, configure APK buildType in eas.json

--- admin/.next/static/chunks/app/layout-f7ad9a70c77050f7.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{3894:function(e,n,i){Promise.resolve().then(i.t.bind(i,7960,23)),Promise.resolve().then(i.bind(i,2228)),Promise.resolve().then(i.t.bind(i,2972,23))},7960:function(){}},function(e){e.O(0,[587,717,971,117,744],function(){return e(e.s=3894)}),_N_E=e.O()}]);
